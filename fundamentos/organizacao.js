d66aba7f0a8897ad77ff368624fc898c4694a374
rhomennik <rhomerios2@gmail.com>
2019-03-21 21:42:09 -0300

    Fundamentos clase 1

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,8 @@
+console.log('Teste 1')
+
+{
+    {
+        console.log('Teste 2');
+        console.log('Teste 3') // Default Course
+    }
+}
---
+++ tipagemfraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'Legal';
+
+console.log(qualquer);
+qualquer = 3.1516
+console.log(qualquer);
+console.log(typeof qualquer);
+
+
+// Evite escolher  nomes genericos e siglas
+
+let valor = '';
+let numero = '';
+let pqp = flase; // Produto Quimico  Perigoso .. KKK
