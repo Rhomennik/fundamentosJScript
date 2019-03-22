5574db07b796e0d4538e9760608e9a1a8b730009
rhomennik <rhomerios2@gmail.com>
2019-03-22 15:22:50 -0300

    Adicionado: Aritimenticos, Destrucutring4, Logicos(vazio), relacionais

---
+++ aritimenticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d, ] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const substracao = d - a
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2 // Par ou Impar 1 0
+
+console.log(soma, substracao, multiplicacao, divisao, modulo)
